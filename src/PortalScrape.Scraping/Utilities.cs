7869d2a57f2117a24d5953cd1f8edc728f92e601
Jonas Pauliukevičius <pauliukevicius.jonas@gmail.com>
2014-03-16 15:03:02 +0200

    Added utilities

---
+++ src/PortalScrape.Scraping/Utilities.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using HtmlAgilityPack;
+
+namespace PortalScrape.Scraping
+{
+    public static class Utilities
+    {
+        public static HtmlNode DownloadPage(string url)
+        {
+            var client = new HttpClient();
+            var task = client.GetStringAsync(url);
+            task.Wait();
+            var result = task.Result;
+
+            var doc = new HtmlDocument();
+            doc.LoadHtml(result);
+            return doc.DocumentNode;
+        }
+    }
+}
