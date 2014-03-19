11a9562ca4c448118e7b5fdf9d090d44c11eb1e1
Jonas Pauliukevičius <pauliukevicius.jonas@gmail.com>
2014-03-19 20:25:33 +0200

    ArticleMap

---
+++ src/PortalScrape.DataAccess/Entities/ArticleInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalScrape.DataAccess.Entities
+{
+    public class ArticleInfo
+    {
+    }
+}
--- src/PortalScrape.DataAccess/Mappings/ArticleMap.cs
+++ src/PortalScrape.DataAccess/Mappings/ArticleMap.cs
@@ -8,7 +8,22 @@ namespace PortalScrape.DataAccess.Mappings
         public ArticleMap()
         {
             Id(x => x.Id);
+            Map(x => x.Portal);
+            Map(x => x.RefNo);
+            Map(x => x.DateScraped);
+
+            Map(x => x.Url);
+            Map(x => x.CommentCount);
+            Map(x => x.DatePublished);
+            Map(x => x.DateModified);
+
             Map(x => x.Title);
+            Map(x => x.AuthorName);
+            Map(x => x.Body);
+
+            Map(x => x.Tags);
+            Map(x => x.Keywords);
+            Map(x => x.RelatedArticles);
             
             Table("Articles");
         }
