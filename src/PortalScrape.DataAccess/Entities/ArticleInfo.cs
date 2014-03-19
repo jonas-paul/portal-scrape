4f677a180c4981ddc545d9ff9f28ed949a8e7cad
Jonas Pauliukevičius <pauliukevicius.jonas@gmail.com>
2014-03-19 20:32:02 +0200

    ArticleInfo data entity and mapping

--- src/PortalScrape.DataAccess/Entities/ArticleInfo.cs
+++ src/PortalScrape.DataAccess/Entities/ArticleInfo.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PortalScrape.DataAccess.Entities
 {
     public class ArticleInfo
     {
+        public virtual int Id { get; set; }
+        public virtual Portal Portal { get; set; }
+        public virtual int RefNo { get; set; }
+        public virtual DateTime DateScraped { get; set; }
+
+        public virtual string Url { get; set; }
+        public virtual string Title { get; set; }
+        public virtual int CommentCount { get; set; }
+        public virtual DateTime DatePublished { get; set; }
     }
 }
---
+++ src/PortalScrape.DataAccess/Mappings/ArticleInfoMap.cs
@@ -0,0 +1,23 @@
+﻿using FluentNHibernate.Mapping;
+using PortalScrape.DataAccess.Entities;
+
+namespace PortalScrape.DataAccess.Mappings
+{
+    internal class ArticleInfoMap : ClassMap<ArticleInfo>
+    {
+        public ArticleInfoMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Portal);
+            Map(x => x.RefNo);
+            Map(x => x.DateScraped);
+
+            Map(x => x.Url);
+            Map(x => x.CommentCount);
+            Map(x => x.DatePublished);
+            Map(x => x.Title);
+            
+            Table("ArticleInfos");
+        }
+    }
+}
