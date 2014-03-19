5bfb9fb558286d069f0bbc59b5c6d8f4dbce1432
Jonas Pauliukevičius <pauliukevicius.jonas@gmail.com>
2014-03-19 20:41:35 +0200

    Comment data entity and mapping

---
+++ src/PortalScrape.DataAccess/Entities/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PortalScrape.DataAccess.Entities
+{
+    public class Comment
+    {
+        public virtual int Id { get; set; }
+        public virtual Portal Portal { get; set; }
+        public virtual int RefNo { get; set; }
+        public virtual DateTime DateScraped { get; set; }
+        
+        public virtual int ArticleRefNo { get; set; }
+        public virtual DateTime DateCreated { get; set; }
+        public virtual string IpAddress { get; set; }
+
+        public virtual string UserName { get; set; }
+        public virtual string CommentText { get; set; }
+        public virtual int InResponseToCommentId { get; set; }
+        public virtual int Upvotes { get; set; }
+        public virtual int DownVotes { get; set; }
+    }
+}
---
+++ src/PortalScrape.DataAccess/Mappings/CommentMap.cs
@@ -0,0 +1,28 @@
+﻿using FluentNHibernate.Mapping;
+using PortalScrape.DataAccess.Entities;
+
+namespace PortalScrape.DataAccess.Mappings
+{
+    internal class CommentMap : ClassMap<Comment>
+    {
+        public CommentMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Portal);
+            Map(x => x.RefNo);
+            Map(x => x.DateScraped);
+
+            Map(x => x.ArticleRefNo);
+            Map(x => x.IpAddress);
+            Map(x => x.DateCreated);
+
+            Map(x => x.UserName);
+            Map(x => x.CommentText);
+            Map(x => x.InResponseToCommentId);
+            Map(x => x.Upvotes);
+            Map(x => x.DownVotes);
+            
+            Table("Comments");
+        }
+    }
+}
